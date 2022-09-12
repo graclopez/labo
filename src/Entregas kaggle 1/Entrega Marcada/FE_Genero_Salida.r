d0863ccb73b7b146d9befe46be02ae5c15555051
Lopez <gracielalopez@MACRO.COM.AR>
2022-09-12 10:57:49 -0300

    error en script subido

--- src/Entregas kaggle 1/Entrega Marcada/FE_Genero_Salida.r
+++ src/Entregas kaggle 1/Entrega Marcada/FE_Genero_Salida.r
@@ -23,6 +23,129 @@ dtrain  <- dataset[ foto_mes==202101 ]  #defino donde voy a entrenar
 dapply  <- dataset[ foto_mes==202103 ]  #defino donde voy a aplicar el modelo
 
 
+#####################################################################
+# FE
+#####################################################################
+dtrain[, cprestamos_personales := ifelse(is.na(cprestamos_personales), 0, cprestamos_personales)] 
+dtrain[, cprestamos_prendarios := ifelse(is.na(cprestamos_prendarios), 0, cprestamos_prendarios)] 
+dtrain[, cprestamos_hipotecarios := ifelse(is.na(cprestamos_hipotecarios),  0, cprestamos_hipotecarios)] 
+dtrain[, suma_cprestamos := cprestamos_hipotecarios + cprestamos_prendarios + cprestamos_personales] 
+
+dtrain[, mprestamos_personales := ifelse(is.na(mprestamos_personales), 0, mprestamos_personales)] 
+dtrain[, mprestamos_prendarios := ifelse(is.na(mprestamos_prendarios), 0, mprestamos_prendarios)] 
+dtrain[, mprestamos_hipotecarios := ifelse(is.na(mprestamos_hipotecarios), 0, mprestamos_hipotecarios)]
+dtrain[, suma_mprestamos := mprestamos_hipotecarios + mprestamos_prendarios + mprestamos_personales] 
+
+dtrain[, cpayroll_trx := ifelse(is.na(cpayroll_trx), 0, cpayroll_trx)] 
+dtrain[, cpayroll2_trx := ifelse(is.na(cpayroll2_trx), 0, cpayroll2_trx)] 
+dtrain[, suma_cpayroll := cpayroll_trx + cpayroll2_trx] 
+
+dtrain[, mpayroll:= ifelse(is.na(mpayroll), 0, mpayroll)] 
+dtrain[, mpayroll2 := ifelse(is.na(mpayroll2), 0, mpayroll2)] 
+dtrain[, suma_mpayroll := mpayroll + mpayroll2] 
+
+dtrain[, ccajeros_propios_descuentos := ifelse(is.na(ccajeros_propios_descuentos), 0, ccajeros_propios_descuentos)] 
+dtrain[, ctarjeta_visa_descuentos := ifelse(is.na(ctarjeta_visa_descuentos), 0, ctarjeta_visa_descuentos)] 
+dtrain[, ctarjeta_master_descuentos := ifelse(is.na(ctarjeta_master_descuentos), 0, ctarjeta_master_descuentos)] 
+dtrain[, suma_cdescuentos := ccajeros_propios_descuentos + ctarjeta_visa_descuentos + ctarjeta_master_descuentos] 
+                  
+dtrain[, ccomisiones_mantenimiento := ifelse(is.na(ccomisiones_mantenimiento), 0, ccomisiones_mantenimiento)] 
+dtrain[, ccomisiones_otras := ifelse(is.na(ccomisiones_otras), 0, ccomisiones_otras)] 
+dtrain[, suma_ccomisiones := ccomisiones_otras + ccomisiones_otras] 
+
+dtrain[, mcajeros_propios_descuentos := ifelse(is.na(mcajeros_propios_descuentos), 0, mcajeros_propios_descuentos)] 
+dtrain[, mtarjeta_visa_descuentos := ifelse(is.na(mtarjeta_visa_descuentos), 0, mtarjeta_visa_descuentos)] 
+dtrain[, mtarjeta_master_descuentos := ifelse(is.na(mtarjeta_master_descuentos), 0, mtarjeta_master_descuentos)] 
+dtrain[, suma_mdescuentos := mcajeros_propios_descuentos + mtarjeta_visa_descuentos + mtarjeta_master_descuentos] 
+
+dtrain[, mcomisiones_mantenimiento := ifelse(is.na(mcomisiones_mantenimiento), 0, mcomisiones_mantenimiento)] 
+dtrain[, mcomisiones_otras := ifelse(is.na(mcomisiones_otras), 0, mcomisiones_otras)] 
+dtrain[, suma_mcomisiones := mcomisiones_mantenimiento + mcomisiones_otras] 
+
+dtrain[, mcuentas_saldo := ifelse(is.na(mcuentas_saldo), 0, mcuentas_saldo)] 
+dtrain[, Master_mlimitecompra := ifelse(is.na(Master_mlimitecompra), 0, Master_mlimitecompra)] 
+dtrain[, Visa_mlimitecompra := ifelse(is.na(Visa_mlimitecompra), 0, Visa_mlimitecompra)] 
+
+
+## proporcion entre prestamos y haberes. Si haberes cero -> entre prestamos y saldo cuentas
+dtrain[, p_prestamos := ifelse(suma_mpayroll==0, ifelse(mcuentas_saldo<=0,0,suma_mprestamos/mcuentas_saldo), suma_mprestamos/suma_mpayroll)] 
+
+## proporcion entre limite compra visa  y haberes. Si cero -> entre limite y saldo cuentas
+dtrain[, p_Visa_mlimitecompra := ifelse(suma_mpayroll==0, ifelse(mcuentas_saldo<=0,0,Visa_mlimitecompra/mcuentas_saldo), Visa_mlimitecompra/suma_mpayroll)] 
+
+## proporcion entre limite compra mc  y haberes. Si cero -> entre limite y saldo cuentas
+dtrain[, p_Master_mlimitecompra := ifelse(suma_mpayroll==0, ifelse(mcuentas_saldo<=0,0,Master_mlimitecompra/mcuentas_saldo), Master_mlimitecompra/suma_mpayroll)] 
+
+
+
+dapply[, cprestamos_personales := ifelse(is.na(cprestamos_personales), 0, cprestamos_personales)] 
+dapply[, cprestamos_prendarios := ifelse(is.na(cprestamos_prendarios), 0, cprestamos_prendarios)] 
+dapply[, cprestamos_hipotecarios := ifelse(is.na(cprestamos_hipotecarios),  0, cprestamos_hipotecarios)] 
+dapply[, suma_cprestamos := cprestamos_hipotecarios + cprestamos_prendarios + cprestamos_personales] 
+
+dapply[, mprestamos_personales := ifelse(is.na(mprestamos_personales), 0, mprestamos_personales)] 
+dapply[, mprestamos_prendarios := ifelse(is.na(mprestamos_prendarios), 0, mprestamos_prendarios)] 
+dapply[, mprestamos_hipotecarios := ifelse(is.na(mprestamos_hipotecarios), 0, mprestamos_hipotecarios)]
+dapply[, suma_mprestamos := mprestamos_hipotecarios + mprestamos_prendarios + mprestamos_personales] 
+
+dapply[, cpayroll_trx := ifelse(is.na(cpayroll_trx), 0, cpayroll_trx)] 
+dapply[, cpayroll2_trx := ifelse(is.na(cpayroll2_trx), 0, cpayroll2_trx)] 
+dapply[, suma_cpayroll := cpayroll_trx + cpayroll2_trx] 
+
+dapply[, mpayroll:= ifelse(is.na(mpayroll), 0, mpayroll)] 
+dapply[, mpayroll2 := ifelse(is.na(mpayroll2), 0, mpayroll2)] 
+dapply[, suma_mpayroll := mpayroll + mpayroll2] 
+
+dapply[, ccajeros_propios_descuentos := ifelse(is.na(ccajeros_propios_descuentos), 0, ccajeros_propios_descuentos)] 
+dapply[, ctarjeta_visa_descuentos := ifelse(is.na(ctarjeta_visa_descuentos), 0, ctarjeta_visa_descuentos)] 
+dapply[, ctarjeta_master_descuentos := ifelse(is.na(ctarjeta_master_descuentos), 0, ctarjeta_master_descuentos)] 
+dapply[, suma_cdescuentos := ccajeros_propios_descuentos + ctarjeta_visa_descuentos + ctarjeta_master_descuentos] 
+                  
+dapply[, ccomisiones_mantenimiento := ifelse(is.na(ccomisiones_mantenimiento), 0, ccomisiones_mantenimiento)] 
+dapply[, ccomisiones_otras := ifelse(is.na(ccomisiones_otras), 0, ccomisiones_otras)] 
+dapply[, suma_ccomisiones := ccomisiones_otras + ccomisiones_otras] 
+
+dapply[, mcajeros_propios_descuentos := ifelse(is.na(mcajeros_propios_descuentos), 0, mcajeros_propios_descuentos)] 
+dapply[, mtarjeta_visa_descuentos := ifelse(is.na(mtarjeta_visa_descuentos), 0, mtarjeta_visa_descuentos)] 
+dapply[, mtarjeta_master_descuentos := ifelse(is.na(mtarjeta_master_descuentos), 0, mtarjeta_master_descuentos)] 
+dapply[, suma_mdescuentos := mcajeros_propios_descuentos + mtarjeta_visa_descuentos + mtarjeta_master_descuentos] 
+
+dapply[, mcomisiones_mantenimiento := ifelse(is.na(mcomisiones_mantenimiento), 0, mcomisiones_mantenimiento)] 
+dapply[, mcomisiones_otras := ifelse(is.na(mcomisiones_otras), 0, mcomisiones_otras)] 
+dapply[, suma_mcomisiones := mcomisiones_mantenimiento + mcomisiones_otras] 
+
+dapply[, mcuentas_saldo := ifelse(is.na(mcuentas_saldo), 0, mcuentas_saldo)] 
+dapply[, Master_mlimitecompra := ifelse(is.na(Master_mlimitecompra), 0, Master_mlimitecompra)] 
+dapply[, Visa_mlimitecompra := ifelse(is.na(Visa_mlimitecompra), 0, Visa_mlimitecompra)] 
+
+
+## proporcion entre prestamos y haberes. Si haberes cero -> entre prestamos y saldo cuentas
+dapply[, p_prestamos := ifelse(suma_mpayroll==0, ifelse(mcuentas_saldo<=0,0,suma_mprestamos/mcuentas_saldo), suma_mprestamos/suma_mpayroll)] 
+
+## proporcion entre limite compra visa  y haberes. Si cero -> entre limite y saldo cuentas
+dapply[, p_Visa_mlimitecompra := ifelse(suma_mpayroll==0, ifelse(mcuentas_saldo<=0,0,Visa_mlimitecompra/mcuentas_saldo), Visa_mlimitecompra/suma_mpayroll)] 
+
+## proporcion entre limite compra mc  y haberes. Si cero -> entre limite y saldo cuentas
+dapply[, p_Master_mlimitecompra := ifelse(suma_mpayroll==0, ifelse(mcuentas_saldo<=0,0,Master_mlimitecompra/mcuentas_saldo), Master_mlimitecompra/suma_mpayroll)] 
+
+dtrain[,c("cprestamos_personales","cprestamos_prendarios","cprestamos_hipotecarios",
+                    "mprestamos_personales","mprestamos_prendarios","mprestamos_hipotecarios",
+                    "cpayroll_trx","cpayroll2_trx",
+                    "mpayroll","mpayroll2",
+                    "ccajeros_propios_descuentos","ctarjeta_visa_descuentos","ctarjeta_master_descuentos",
+                    "ccomisiones_mantenimiento","ccomisiones_otras",
+                    "mcajeros_propios_descuentos","mtarjeta_visa_descuentos","mtarjeta_master_descuentos",
+                    "mcomisiones_mantenimiento","mcomisiones_otras"):=NULL]
+
+dapply[,c("cprestamos_personales","cprestamos_prendarios","cprestamos_hipotecarios",
+                    "mprestamos_personales","mprestamos_prendarios","mprestamos_hipotecarios",
+                    "cpayroll_trx","cpayroll2_trx",
+                    "mpayroll","mpayroll2",
+                    "ccajeros_propios_descuentos","ctarjeta_visa_descuentos","ctarjeta_master_descuentos",
+                    "ccomisiones_mantenimiento","ccomisiones_otras",
+                    "mcajeros_propios_descuentos","mtarjeta_visa_descuentos","mtarjeta_master_descuentos",
+                    "mcomisiones_mantenimiento","mcomisiones_otras"):=NULL]
+
 # Entreno el modelo
 # obviamente rpart no puede ve  clase_ternaria para predecir  clase_binaria
 #  #no utilizo Visa_mpagado ni  mcomisiones_mantenimiento por drifting
@@ -30,10 +153,10 @@ dapply  <- dataset[ foto_mes==202103 ]  #defino donde voy a aplicar el modelo
 modelo  <- rpart(formula=   "clase_binaria ~ . -clase_ternaria",
                  data=      dtrain,  #los datos donde voy a entrenar
                  xval=         5,
-                 cp=          -0.52,#  -0.89
-                 minsplit=  959,   # 621
-                 minbucket=  252,   # 309
-                 maxdepth=     9 )  #  12
+                 cp=          -0.37,#  -0.89
+                 minsplit=  1092,   # 621
+                 minbucket=  278,   # 309
+                 maxdepth=     8 )  #  12
 
 
 #----------------------------------------------------------------------------
@@ -81,8 +204,7 @@ dfinal[ , azar := runif( nrow(dapply) ) ]
 setorder( dfinal, -prob_SI, azar )
 
 
-dir.create( "./exp/" )
-dir.create( "./exp/KA4120" )
+#dir.create( "./exp/FEComp1/" )
 
 
 for( corte  in  c( 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000 ) )
@@ -93,7 +215,7 @@ for( corte  in  c( 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000 ) )
 
 
   fwrite( dfinal[ , list(numero_de_cliente, Predicted) ], #solo los campos para Kaggle
-           file= paste0( "./exp/KA4120/KA4120_005_",  corte, ".csv"),
+           file= paste0( "./exp/FEComp1/FE_002_",  corte, ".csv"),
            sep=  "," )
 }
 
